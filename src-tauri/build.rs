d387acb90f3f9c181cd0ef3b5d7b808f4e5d2920
zhy2006 <fengyunjack@126.com>
2026-03-27 19:22:19 +0800

    feat: Add app icons, configure bundler, and set up base HTML structure

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,12 @@
 fn main() {
+    println!("cargo:rerun-if-changed=icons/icon.icns");
+    println!("cargo:rerun-if-changed=icons/icon.png");
+    println!("cargo:rerun-if-changed=icons/icon_32x32.png");
+    println!("cargo:rerun-if-changed=icons/icon_128x128.png");
+    println!("cargo:rerun-if-changed=icons/icon_128x128@2x.png");
+    println!("cargo:rerun-if-changed=icons/icon_256x256.png");
+    println!("cargo:rerun-if-changed=icons/icon_256x256@2x.png");
+    println!("cargo:rerun-if-changed=icons/icon_512x512.png");
+    println!("cargo:rerun-if-changed=icons/icon_512x512@2x.png");
     tauri_build::build()
 }
